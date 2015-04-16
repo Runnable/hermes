1e76f31fea630ceedab3d7fe450accd8198f4ed7
Casey Flynn <cflynn.us@gmail.com>
2015-04-16 16:58:28 -0700

    fix arguments pub/sub, enforcing pub/sub to predefined queues

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ require('loadenv')();
 
 var EventEmitter = require('events').EventEmitter;
 var amqplib = require('amqplib/callback_api');
+var async = require('async');
 var debug = require('debug')('hermes:index');
 var hasKeypaths = require('101/has-keypaths');
 var util = require('util');
 
-var generalQueueName = 'general';
+var queues = [
+  'container-create'
+];
 var hermes;
 
 /**
@@ -25,8 +28,10 @@ var hermes;
 function Hermes (opts) {
   var requiredOpts = ['hostname', 'port', 'username', 'password'];
   if (!hasKeypaths(opts, requiredOpts)) {
-    throw new Error('Hermes missing required arguments. Opts  must '+
-                    'include: '+requiredOpts.join(', '));
+    throw new Error('Hermes missing required arguments. Supplied opts '+
+                    Object.prototype.keys(opts).join(', ')+
+                    '. Opts must include: '+
+                    requiredOpts.join(', '));
   }
   var _this = this;
   this.channel = null;
@@ -52,9 +57,10 @@ function Hermes (opts) {
        * Durable queue: https://www.rabbitmq.com/tutorials/tutorial-two-python.html
        * (Message Durability)
        */
-      ch.assertQueue(generalQueueName, {durable: true}, function (err) {
+      async.forEach(queues, function forEachQueue (queueName, cb) {
+        ch.assertQueue(queueName, {durable: true}, cb);
+      }, function done (err) {
         if (err) { throw err; }
-        debug('rabbitmq queue: "'+generalQueueName+'" created');
         _this.channel = ch;
         _this.emit('ready');
       });
@@ -62,42 +68,46 @@ function Hermes (opts) {
   });
   this.on('ready', function () {
     debug('hermes ready');
-    this.publishQueue.map(publish, _this);
-    this.subscribeQueue.map(subscribe, _this);
+    _this.publishQueue.forEach(function (args) {
+      publish.apply(_this, args);
+    });
+    _this.subscribeQueue.forEach(function (args) {
+      subscribe.apply(_this, args);
+    });
   });
-  this.on('publish', function (data) {
-    debug('hermes publish', data);
+  this.on('publish', function (jobName, data) {
+    debug('hermes publish', jobName, data);
     if (_this.channel) {
-      publish(data);
+      publish(jobName, data);
     }
     else {
-      _this.publishQueue.push(data);
+      _this.publishQueue.push(Array.prototype.slice.call(arguments));
     }
   });
-  this.on('subscribe', function (data) {
-    debug('hermes subscribe', data);
+  this.on('subscribe', function (jobName, cb) {
+    debug('hermes subscribe', jobName);
     if (_this.channel) {
-      subscribe(data);
+      subscribe(jobName, cb);
     }
     else {
-      _this.subscribeQueue.push(data);
+      _this.subscribeQueue.push(Array.prototype.slice.call(arguments));
     }
   });
   /**
    * @param {Object} data
    * @return null
    */
-  function publish (data) {
-    debug('channel.sendToQueue', generalQueueName, data);
-    _this.channel.sendToQueue(generalQueueName, data);
+  function publish (queueName, data) {
+    debug('channel.sendToQueue', queueName, data);
+    _this.channel.sendToQueue(queueName, data);
   }
   /**
    * @param {Object} data
    * @return null
    */
-  function subscribe (cb) {
-    debug('channel.consume', generalQueueName);
-    _this.channel.consume(generalQueueName, cb);
+  function subscribe (queueName, cb) {
+    debug('channel.consume', queueName);
+    _this.channel.consume(queueName, cb);
   }
   return this;
 }
@@ -105,20 +115,28 @@ function Hermes (opts) {
 util.inherits(Hermes, EventEmitter);
 
 /**
+ * @param {String} jobName
  * @param {Object} data
  * @return this
  */
-Hermes.prototype.publish = function (data) {
-  this.emit('publish', data);
+Hermes.prototype.publish = function (jobName, data) {
+  if (!~queues.indexOf(jobName)) {
+    throw new Error('attempting to publish to invalid job type: '+jobName);
+  }
+  this.emit('publish', jobName, data);
   return this;
 };
 
 /**
+ * @param {String} jobName
  * @param {Object} data
  * @return this
  */
-Hermes.prototype.subscribe = function (data) {
-  this.emit('subscribe', data);
+Hermes.prototype.subscribe = function (jobName, cb) {
+  if (!~queues.indexOf(jobName)) {
+    throw new Error('attempting to subscribe to invalid job type: '+jobName);
+  }
+  this.emit('subscribe', jobName, cb);
   return this;
 };
 
