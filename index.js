79590973f167a975a711abd5825c601912d2b178
Casey Flynn <cflynn.us@gmail.com>
2015-04-29 17:01:11 -0700

    removing ENV check disable publish

--- index.js
+++ index.js
@@ -141,10 +141,6 @@ util.inherits(Hermes, EventEmitter);
  */
 Hermes.prototype.publish = function (queueName, data) {
   /*jshint maxcomplexity:7 */
-  if (process.env.DISABLE_HERMES_PUBLISH) {
-    debug('DISABLE_HERMES_PUBLISH', queueName, data);
-    return this;
-  }
   if (!~queues.indexOf(queueName)) {
     throw new Error('attempting to publish to invalid queue: '+queueName);
   }
